0b5bb9dd7ee154f5fd35ee1d8a15b7d5c1d4b662
gustn16113 <gustn16113@naver.com>
2020-02-19 12:01:01 +0900

    0219

---
+++ Swea/Solution_1210_Ladder1.java
@@ -0,0 +1,69 @@
+package swea.d4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_1210_Ladder1 {
+	
+	public static int[][] map;
+	public static boolean[][] visited;
+	public static int[][] dir = {{0,1}, {0,-1}, {1,0}}; 
+	public static boolean stop = false;
+	public static int ans = 0;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		for(int t=1; t<=10; t++) {
+			br.readLine();
+			ans = 0;
+			stop = false;
+			map = new int[100][100];
+			visited = new boolean[100][100];
+			for(int i=0; i<100; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j=0; j<100; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			for(int i=0; i<100; i++) {
+				if(map[0][i] == 1) {
+					dfs(0, i, i);
+				}
+			}
+			System.out.println("#" + t + " " + ans);
+		}
+		
+//		for(int i=0; i<100; i++) {
+//			for(int j=0; j<100; j++) {
+//				System.out.print(map[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
+	}
+	
+	public static void dfs(int r, int c, int start) {
+		boolean d = false;
+		for(int i=0; i<3; i++) {
+			int nr = r + dir[i][0];
+			int nc = c + dir[i][1];
+			if(nr > -1 && nc > -1 && nr < 100 && nc < 100) {
+				if(stop) {
+					return;
+				}
+				if(map[nr][nc] == 2) {
+					stop = true;
+					ans = start;
+					return;
+				}
+				if(map[nr][nc] == 1 && !visited[nr][nc] && !d) {
+					visited[nr][nc] = true;
+					dfs(nr, nc, start);
+					visited[nr][nc] = false;
+					d = true;
+				}
+			}
+		}
+	}
+}
