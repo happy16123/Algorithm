8920f144eb9595202a39eb15513509c717edf28c
happy16123 <51432536+happy16123@users.noreply.github.com>
2020-03-14 23:04:25 +0900

    0314

---
+++ Baek/Main_2579_계단오르기.java
@@ -0,0 +1,30 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_2579_계단오르기 {
+	
+	public static int[] data;
+	public static int[] dp;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		data = new int[N+1];
+		dp = new int[N+1];
+		
+		for(int i=1; i<=N; i++) {
+			data[i] = Integer.parseInt(br.readLine());
+		}
+		
+		dp[1] = data[1];
+		dp[2] = data[1] + data[2];
+		
+		for(int i=3; i<=N; i++) {
+			dp[i] = Math.max(dp[i-2] + data[i], dp[i-3] + data[i-1] + data[i]);
+		}
+		
+		System.out.println(dp[N]);
+	}
+}
