13bc7f12b0feb575b65d2d10f7c2c3e341b0a42a
happy16123 <51432536+happy16123@users.noreply.github.com>
2020-04-11 12:48:36 +0900

    0411

---
+++ Baek/Main_9372_상근이의여행.java
@@ -0,0 +1,23 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_9372_상근이의여행 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for (int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			for (int i = 0; i < M; i++) {
+				br.readLine();
+			}
+			System.out.println(N-1);
+		}
+	}
+}
